1f6ac22bf24802af775ad85e6cf6555316e983a9
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-05-19 19:41:27 +0600

    Add Shop By Category

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+
 
 // middleware
 app.use(cors());
@@ -11,9 +12,8 @@ app.use(express.json());
 
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6rgz80t.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,16 +21,34 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('kidCar').collection('shopByCategory');
+
+
+
+    // Shop By Category
+
+    app.get('/category', async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+   
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,14 +56,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
 app.get('/', (req, res) => {
-  res.send('kids playing with toy car')
-})
+  res.send('kids playing with toy car');
+});
 
 app.listen(port, () => {
   console.log(`kids car server is running on port: ${port}`);
-})
+});
